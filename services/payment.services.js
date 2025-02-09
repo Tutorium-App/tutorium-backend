a03fdf70c4c67d16606d3f7b97e1acac49f50e51
Law-son <buabassahlawson@gmail.com>
2025-02-09 16:24:38 +0000

    fix: charges error

--- services/payment.services.js
+++ services/payment.services.js
@@ -46,7 +46,7 @@ class PaymentServices {
                     } else {
                         if (body && body.data && body.data.reference) {
                             const reference = body.data.reference;
-                            await PaymentDetailsServices.storePaymentDetails(reference, paymentData.tutorialID, paymentData.tutorID, paymentData.tutorName, paymentData.tutorEmail, paymentData.studentID, paymentData.studentName, paymentData.studentNumber, paymentData.studentEmail, paymentData.tutorNumber, paymentData.tutorialTitle, paymentData.amount, paymentData.category, paymentData.imageURL, paymentData.requestID, paymentData.isRequest);
+                            await PaymentDetailsServices.storePaymentDetails(reference, paymentData.tutorialID, paymentData.tutorID, paymentData.tutorName, paymentData.tutorEmail, paymentData.studentID, paymentData.studentName, paymentData.studentNumber, paymentData.studentEmail, paymentData.tutorNumber, paymentData.tutorialTitle, finalCost, paymentData.category, paymentData.imageURL, paymentData.requestID, paymentData.isRequest);
                             resolve(body); // Resolve with the entire body to access authorization_url
                         } else {
                             console.error('Invalid response structure from Paystack');
